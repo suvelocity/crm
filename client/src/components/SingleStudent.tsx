c505b8500a6a7a33c50a88a0ffd9ab9c4f9b43bb
AmirDebbie <67858073+AmirDebbie@users.noreply.github.com>
2020-11-09 13:56:44 +0200

    removed import of button

--- client/src/components/SingleStudent.tsx
+++ client/src/components/SingleStudent.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
