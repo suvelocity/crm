78283d35e4af565c392fbf431611e36c4a302311
ZBejavu <67857471+ZBejavu@users.noreply.github.com>
2020-11-22 00:05:42 +0200

    trying npm run dev instead of pm2

--- server/app.ts
+++ server/app.ts
@@ -4,12 +4,10 @@ import helmet from "helmet";
 import path from "path";
 
 const app = express();
-
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(helmet());
-
-// app.use(express.static("../client/build"));
 
 // app.get("/", (req: Request, res: Response) => {
 //   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
