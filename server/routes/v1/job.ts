fed8f7ed200e67039a3a4ef0003b3f23dca25379
ZBejavu <67857471+ZBejavu@users.noreply.github.com>
2020-11-22 11:08:05 +0200

    checking

--- server/routes/v1/job.ts
+++ server/routes/v1/job.ts
@@ -22,6 +22,7 @@ router.get("/all", async (req: Request, res: Response) => {
     });
     res.json(jobs);
   } catch (err) {
+    console.log(err);
     res.status(500).send("error occurred");
   }
 });
