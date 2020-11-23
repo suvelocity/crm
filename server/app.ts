e044f5b1ffd533fef35662e8256964e38e8ce13a
ZBejavu <67857471+ZBejavu@users.noreply.github.com>
2020-11-23 22:04:00 +0200

    checking the build

--- server/app.ts
+++ server/app.ts
@@ -4,15 +4,20 @@ import helmet from "helmet";
 import path from "path";
 
 const app = express();
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(helmet());
-// app.use(express.static(path.join(__dirname, "../client/build")));
+
+// app.use("/api", require("./routes"));
+// app.use(express.static(path.join(__dirname, "..", "client", "build")));
+// app.use("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+// });
 
 app.use("/api", require("./routes"));
-app.use(express.static(path.join(__dirname, "..", "client", "build")));
-app.use("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 
