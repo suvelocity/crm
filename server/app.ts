0039a2bc0b5baec9125af16928d5bcebf8b0e721
ZBejavu <67857471+ZBejavu@users.noreply.github.com>
2020-11-25 21:21:37 +0200

    helmet test

--- server/app.ts
+++ server/app.ts
@@ -7,7 +7,11 @@ const app = express();
 app.use(express.static("../client/build"));
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 
 // app.use("/api", require("./routes"));
 // app.use(express.static(path.join(__dirname, "..", "client", "build")));
