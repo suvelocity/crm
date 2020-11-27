13ace97c9c6162c2fdc37b3a1d2a997c1bd0f69e
listguy <nitzanjl@gmail.com>
2020-11-27 17:02:47 +0200

    multi line text fields modifications
    fixed and added client validations for length (500 chars).
    added a migration to DB but encountered an error.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css";
 import AddStudent from "./components/studentRelated/AddStudent";
 import AllStudents from "./components/studentRelated/AllStudents";
 import SingleStudent from "./components/studentRelated/SingleStudent";
