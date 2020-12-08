6039d914f3cecabfccaf2b9ac8e6695bbe1ce1b0
trodelgal <67858041+trodelgal@users.noreply.github.com>
2020-12-08 13:28:51 +0200

    start end points

--- server/routes/v1/index.ts
+++ server/routes/v1/index.ts
@@ -5,10 +5,12 @@ const router = Router();
 const unknownEndpoint = (req: Request, res: Response) => {
   res.status(404).send({ error: "unknown endpoint" });
 };
+
 router.use("/class", require("./class"));
 router.use("/job", require("./job"));
 router.use("/student", require("./student"));
 router.use("/event", require("./event"));
+router.use("/mentor", require("./mentor"));
 
 router.use(unknownEndpoint);
 module.exports = router;
