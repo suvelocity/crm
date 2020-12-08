ff49c6d966a3414d53798d3c81a3d14aeceda8b1
Matan Greenvlad <mgk2798@gmail.com>
2020-12-08 15:59:55 +0200

    update meetings server

--- server/routes/v1/mentor/meeting.ts
+++ server/routes/v1/mentor/meeting.ts
@@ -1,7 +1,11 @@
 import { Request, Response , Router } from "express";
+import { Sequelize } from "sequelize/types";
 //@ts-ignore
 import { Student, Job, Event, Class, Mentor, Meeting } from "../../../models";
 import { IMentor } from "../../../types";
+import { meetingSchemaToPut } from "../../../validations";
+const { Op } = require('Sequelize')
+const oneDay = 1000 * 60 * 60 * 24
 
 const router = Router();
 
@@ -30,26 +34,83 @@ router.get('/class/:id', async (req: Request, res: Response) => {
 
 // get student meets:
 router.get('/student/:id', async (req: Request, res: Response) => {
-    try{
-        const studentMeets:any[] = await Student.findOne({
-            attributes:["id", "firstName", "lastName"],
-            where:{id:req.params.id},
-            include:[
+    try {
+        const studentMeets: any[] = await Student.findOne({
+            attributes: ["id", "firstName", "lastName"],
+            where: { id: req.params.id },
+            include: [
                 {
                     model: Mentor
                 },
                 {
                     model: Meeting,
-                    attributes:["date"],
+                    attributes: ["date"],
                 },
             ]
         });
 
         res.json(studentMeets);
-    }catch(err){
+    } catch (err) {
         res.status(500).json({ error: err.message });
     }
-})
+});
+
+router.get('/byday/:day/:offset', async (req: Request, res: Response) => {
+    try {
+        const startDate: Date = new Date(req.params.day)
+        const daysGap: number = parseInt(req.params.offset)
+        const endDate: Date = new Date(startDate.getTime() + (oneDay * daysGap))
+        const meetings: any[] = await Meeting.findAll({
+            where: {
+                date: {
+                    [Op.lt]: endDate,
+                    [Op.gt]: startDate
+                  }
+            },
+            include: [
+                {
+                    model: Student,
+                    attributes: ["first_name", "last_name", "email"],
+                },
+                {
+                    model: Mentor,
+                    attributes: ["name", "email"],
+                }
+            ]
+                
+        });
+        res.json(meetings);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.put("/:id", async (req: Request, res: Response) => {
+    const { error } = meetingSchemaToPut.validate(req.body);
+    if (error) return res.status(400).json(error);
+    try {
+
+        const updated = await Meeting.update({
+            place: req.body.place,
+            date: new Date(req.body.date)
+      }, {
+        where: { id: req.params.id },
+      });
+      if (updated[0] === 1) return res.json({ message: "Meeting updated" });
+      res.status(404).json({ error: "Meeting not found" });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+  
+router.get("/all", async (req: Request, res: Response) => {
+    try {
+      const meetings: any[] = await Meeting.findAll();
+      res.json(meetings);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+});
 
 
 // post new meet:
