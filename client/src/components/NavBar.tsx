8a24ef347d918ffd9774915f8cafa8093dfa73b4
AmirDebbie <67858073+AmirDebbie@users.noreply.github.com>
2020-12-10 21:07:51 +0200

    fixed signout button

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import {
   AppBar,
   Toolbar,
@@ -7,17 +7,31 @@ import {
   Drawer,
 } from "@material-ui/core";
 import { Menu } from "@material-ui/icons";
-import SignOutButton from "./auth/SignOutButton";
 import PeopleIcon from "@material-ui/icons/People";
 import WorkIcon from "@material-ui/icons/Work";
 import BusinessIcon from "@material-ui/icons/Business";
 import styled from "styled-components";
 import { StyledLink } from "../styles/styledComponents";
+import { useHistory } from "react-router-dom";
 import ClassIcon from "@material-ui/icons/Class";
 import TimelineIcon from "@material-ui/icons/Timeline";
+import network from "../helpers/network";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import { getRefreshToken, AuthContext } from "../helpers";
 
 function NavAppBar() {
   const [open, setOpen] = useState(false);
+  const history = useHistory();
+  //@ts-ignore
+  const { setUser } = useContext(AuthContext);
+
+  const signOut = async () => {
+    await network.post("/api/v1/auth/signout", {
+      refreshToken: getRefreshToken(),
+    });
+    setUser(null);
+    history.push("/");
+  };
 
   const handleDrawer = () => {
     setOpen(true);
@@ -25,62 +39,63 @@ function NavAppBar() {
 
   return (
     <div>
-      <AppBar position='static'>
+      <AppBar position="static">
         <Toolbar>
           <IconButton
             onClick={handleDrawer}
-            color='inherit'
-            edge='start'
-            aria-label='menu'
+            color="inherit"
+            edge="start"
+            aria-label="menu"
           >
             <Menu />
           </IconButton>
-          <StyledLink to='/student/all'>
-            <Typography variant='h4'>CRM</Typography>
+          <StyledLink to="/student/all">
+            <Typography variant="h4">CRM</Typography>
           </StyledLink>
         </Toolbar>
       </AppBar>
       <Drawer
-        anchor='left'
+        anchor="left"
         open={open}
         onClose={() => {
           setOpen(false);
         }}
       >
         <StyledDrawer>
-          <StyledLink to='/student/all'>
+          <StyledLink to="/student/all">
             <DrawerItem onClick={() => setOpen(false)}>
               Students
               <PeopleIcon style={{ position: "absolute", right: 10 }} />
             </DrawerItem>
           </StyledLink>
-          <StyledLink to='/job/all'>
+          <StyledLink to="/job/all">
             <DrawerItem onClick={() => setOpen(false)}>
               Jobs
               <WorkIcon style={{ position: "absolute", right: 10 }} />
             </DrawerItem>
           </StyledLink>
-          <StyledLink to='/class/all'>
+          <StyledLink to="/class/all">
             <DrawerItem onClick={() => setOpen(false)}>
               Classes
               <ClassIcon style={{ position: "absolute", right: 10 }} />
             </DrawerItem>
           </StyledLink>
-          <StyledLink to='/company/all'>
+          <StyledLink to="/company/all">
             <DrawerItem onClick={() => setOpen(false)}>
               Companies
               <BusinessIcon style={{ position: "absolute", right: 10 }} />
             </DrawerItem>
           </StyledLink>
-          <StyledLink to='/process/all'>
+          <StyledLink to="/process/all">
             <DrawerItem onClick={() => setOpen(false)}>
               Processes
               <TimelineIcon style={{ position: "absolute", right: 10 }} />
             </DrawerItem>
-            <DrawerItem onClick={() => setOpen(false)}>
-              <SignOutButton style={{ position: "absolute", right: 10 }} />
-            </DrawerItem>
           </StyledLink>
+          <DrawerItem onClick={signOut}>
+            Sign Out
+            <ExitToAppIcon style={{ position: "absolute", right: 10 }} />
+          </DrawerItem>
         </StyledDrawer>
       </Drawer>
     </div>
