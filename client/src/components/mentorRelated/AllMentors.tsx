8b1ec15fb8af67c32e48147de10da510b4c2f727
tomertv2 <tomertv2@gmail.com>
2020-12-10 12:25:01 +0200

    Add basic all mentors table

---
+++ client/src/components/mentorRelated/AllMentors.tsx
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from 'react';
+import network from '../../helpers/network';
+import {
+  H1,
+  Wrapper,
+  TitleWrapper,
+  StyledLink,
+  Center,
+  StyledSpan,
+  TableHeader,
+  StyledUl,
+  StyledDiv,
+} from '../../styles/styledComponents';
+import PersonIcon from '@material-ui/icons/Person';
+import { IMentor } from '../../typescript/interfaces';
+import { Loading } from 'react-loading-wrapper';
+import 'react-loading-wrapper/dist/index.css';
+import { formatPhone, capitalize } from '../../helpers/general';
+
+function AllMentors() {
+  const [mentors, setMentors] = useState<IMentor[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await network.get('/api/v1/mentor/mentor');
+      //   const newClassNames: string[] = Array.from(
+      //     new Set(data.map((mentor: IMentor) => mentor.name))
+      //   );
+      //   const newCourseNames: string[] = Array.from(
+      //     new Set(data.map((student: IStudent) => student.Class.course))
+      //   );
+      //   const newFullNames: string[] = Array.from(
+      //     new Set(
+      //       data.map(
+      //         (student: IStudent) => student.firstName + " " + student.lastName
+      //       )
+      //     )
+      //   );
+      setMentors(data);
+      setLoading(false);
+    })();
+  }, []);
+
+  return (
+    <Wrapper width='80%'>
+      <Center>
+        <TitleWrapper>
+          <H1 color='#2c6e3c'>All Mentors</H1>
+        </TitleWrapper>
+        <br />
+        {/* <StyledLink to="/mentor/new">
+          <Button
+            variant="contained"
+            style={{
+              backgroundColor: "#2c6e3c",
+              color: "white",
+              marginLeft: 10,
+            }}
+          >
+            new project
+          </Button>
+        </StyledLink> */}
+      </Center>
+      <br />
+      <Loading loading={loading} size={30}>
+        <StyledUl>
+          {mentors && (
+            <li>
+              <TableHeader>
+                <PersonIcon />
+                <StyledSpan weight='bold'>Name</StyledSpan>
+                <StyledSpan weight='bold'>Company</StyledSpan>
+                <StyledSpan weight='bold'>Email</StyledSpan>
+                <StyledSpan weight='bold'>Phone</StyledSpan>
+                <StyledSpan weight='bold'>Address</StyledSpan>
+                <StyledSpan weight='bold'>Job</StyledSpan>
+                <StyledSpan weight='bold'>Available</StyledSpan>
+                <StyledSpan weight='bold'>Gender</StyledSpan>
+              </TableHeader>
+            </li>
+          )}
+          {mentors &&
+            mentors.map((mentor) => (
+              <li>
+                <StyledLink color='black' to={`/mentor/${mentor?.id}`}>
+                  <StyledDiv>
+                    <PersonIcon />
+                    <StyledSpan weight='bold'>
+                      {capitalize(mentor.name)}
+                      {/* &nbsp; */}
+                      {/* {capitalize(student.lastName)} */}
+                    </StyledSpan>
+                    <StyledSpan>{capitalize(mentor.company)}</StyledSpan>
+                    <StyledSpan>{mentor.email}</StyledSpan>
+                    <StyledSpan>{formatPhone(mentor.phone)}</StyledSpan>
+                    <StyledSpan>{mentor.address}</StyledSpan>
+                    <StyledSpan>{mentor.job}</StyledSpan>
+                    <StyledSpan>{mentor.available ? 'yes' : 'no'}</StyledSpan>
+                    <StyledSpan>{mentor.gender}</StyledSpan>
+                  </StyledDiv>
+                </StyledLink>
+              </li>
+            ))}
+        </StyledUl>
+      </Loading>
+    </Wrapper>
+  );
+}
+
+export default AllMentors;
