f72bba459baf6c9878a6c3ed407bed4f480f5609
Matan Greenvlad <mgk2798@gmail.com>
2020-12-13 13:17:41 +0200

    partly assign mentors

---
+++ client/src/components/mentorRelated/Modal.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }),
+);
+
+interface ModalProps {
+    open: boolean,
+    setOpen: Function,
+    modalBody: any
+}
+
+export default function SimpleModal(props: ModalProps) {
+  const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = React.useState(getModalStyle);
+
+  const handleOpen = () => {
+    props.setOpen(true);
+  };
+
+  const handleClose = () => {
+    props.setOpen(false);
+  };
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      {props.modalBody}
+    </div>
+  );
+
+  return (
+    <div>
+      <Modal
+        open={props.open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
+    </div>
+  );
+}
