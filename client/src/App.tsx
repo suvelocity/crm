694869f8dddbab37ec7faab942e29c65c70ec6ec
ShaharEli <shahar.e3@gmail.com>
2020-12-13 14:34:21 +0200

    dark theme added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,33 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import { AuthContext, getRefreshToken } from "./helpers";
+import { AuthContext, getRefreshToken, theme, ThemeContext } from "./helpers";
 import axios from "axios";
-import { IUser } from "./typescript/interfaces";
+import { IUser, ThemeType } from "./typescript/interfaces";
 //@ts-ignore
 import { PublicRoutes, AdminRoutes, StudentRoutes } from "./routes";
-import TeacherRoutes from './routes/TeacherRoutes'
+import TeacherRoutes from "./routes/TeacherRoutes";
 import { Loading } from "react-loading-wrapper";
 import "react-loading-wrapper/dist/index.css";
+import { ThemeProvider } from "styled-components";
 
 function App() {
   const [user, setUser] = useState<IUser | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
+  const [currentTheme, setCurrentTheme] = useState("light");
 
   useEffect(() => {
     (async () => {
       try {
+        const previousTheme = localStorage.getItem("theme");
+        if (previousTheme) {
+          setCurrentTheme(previousTheme);
+        } else {
+          if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            //check default theme of the user
+            setCurrentTheme("dark");
+          }
+        }
         const { data: userData } = await axios.post("/api/v1/auth/token", {
           refreshToken: getRefreshToken(),
           remembered: true,
@@ -32,17 +43,32 @@ function App() {
       setLoading(false);
     })();
   }, []);
-  
+
   const getRoutes = () => {
     if (loading) return <Loading fullPage loading={true} />;
-    if (!user) return <PublicRoutes />; 
+    if (!user) return <PublicRoutes />;
     switch (user.userType) {
       case "admin":
         return <AdminRoutes />;
       case "student":
-        return <StudentRoutes />;
+        return (
+          <ThemeContext.Provider value={{ currentTheme, setCurrentTheme }}>
+            {/* @ts-ignore*/}
+            <ThemeProvider theme={() => theme(currentTheme)}>
+              <StudentRoutes />;
+            </ThemeProvider>
+          </ThemeContext.Provider>
+        );
+
       case "teacher":
-        return <TeacherRoutes />;
+        return (
+          <ThemeContext.Provider value={{ currentTheme, setCurrentTheme }}>
+            {/* @ts-ignore*/}
+            <ThemeProvider theme={() => theme(currentTheme)}>
+              <TeacherRoutes />
+            </ThemeProvider>
+          </ThemeContext.Provider>
+        );
     }
   };
 
---
+++ client/src/helpers/themes/dark.ts
@@ -0,0 +1,10 @@
+export const darkTheme = {
+  colors: {
+    background: '#002B36',
+    sideBar: '#242526',
+    addItem: '#242526',
+    font: 'white',
+    divider: 'red',
+    task: '#003847',
+  },
+};
