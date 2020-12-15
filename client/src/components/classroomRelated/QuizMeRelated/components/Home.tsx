71e0049e6d27587c58a00fb82a141dc187172d5b
Tamer Hamoud <67854476+TamerHa-coder@users.noreply.github.com>
2020-12-15 18:08:35 +0200

    JS files => TS files
    Convering all JS files  to TS files

--- client/src/components/classroomRelated/QuizMeRelated/components/Home.tsx
+++ client/src/components/classroomRelated/QuizMeRelated/components/Home.tsx
@@ -1,21 +1,21 @@
 import React from "react";
-// import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
-// import Header from "./Header";
-// import QuizzesList from "./QuizzesList";
+import Header from "./Header";
+import QuizzesList from "./QuizzesList";
 
-// const useStyles = makeStyles({
-//   container: {
-//     marginTop: "5em",
-//   },
-// });
+const useStyles = makeStyles({
+  container: {
+    marginTop: "5em",
+  },
+});
 
-// export default function Home() {
-//   const classes = useStyles();
-//   return QuizzesList ? (
-//     <>
-//         <Header text='Welcome to QuizMe!'/>
-//         <QuizzesList />
-//     </>
-//   ) : null
-// }
+export default function Home() {
+  const classes = useStyles();
+  return QuizzesList ? (
+    <>
+        <Header text='Welcome to QuizMe!'/>
+        <QuizzesList />
+    </>
+  ) : null
+}
