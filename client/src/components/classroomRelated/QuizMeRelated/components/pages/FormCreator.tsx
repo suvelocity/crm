3803fb0dda2b12e785831a4dd983a5fce923bf0b
Nir Kahana <nirkahana8@gmail.com>
2020-12-20 12:14:58 +0200

    Update FormCreator.tsx

--- client/src/components/classroomRelated/QuizMeRelated/components/pages/FormCreator.tsx
+++ client/src/components/classroomRelated/QuizMeRelated/components/pages/FormCreator.tsx
@@ -11,10 +11,7 @@ import {
 } from "@material-ui/core";
 import network from "../../../../../helpers/network";
 import { IFormExtended, IField } from "../../../../../typescript/interfaces";
-const useStyles = makeStyles((theme) => ({
-  
-}));
-
+const useStyles = makeStyles((theme) => ({}));
 
 export default function FormPage() {
   const classes = useStyles();
@@ -25,7 +22,36 @@ export default function FormPage() {
 
   return (
     <>
-    
+      {/* LABEL */}
+      <div>
+        <label htmlFor={"formName"}>What is the name of your form?</label>
+      </div>
+      {/* INPUT */}
+      <div>
+        <input
+          ref={register({ required: true })}
+          name={"formName"}
+          placeholder="Your answer"
+        />
+        {errors["formName"] && <span>This field is required</span>}
+      </div>
+      
+      {/* LABEL */}
+      <div>
+        <label htmlFor={"formName"}>What is the name of your form?</label>
+      </div>
+      {/* INPUT */}
+      <div>
+        <input
+          ref={register({ required: true })}
+          name={"formName"}
+          placeholder="Your answer"
+        />
+        {errors["formName"] && <span>This field is required</span>}
+      </div>
+
+
+
     </>
   );
 }
