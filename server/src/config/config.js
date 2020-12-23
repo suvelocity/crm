311108bdba549a0952785cfe50f1438658255d69
ZBejavu <67857471+ZBejavu@users.noreply.github.com>
2020-12-23 20:17:58 +0200

    created workflow for test checking before merge

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -13,8 +13,8 @@ module.exports = {
   },
   test: {
     username: "root",
-    password: process.env.MYSQL_PASSWORD,
-    database: "crm_test",
+    password: process.env.MYSQL_PASSWORD || null,
+    database: process.env.MYSQL_DATABASE || "crm_test",
     host: "127.0.0.1",
     dialect: "mysql",
     define: {
